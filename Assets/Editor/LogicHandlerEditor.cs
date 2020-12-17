74fe7cec3f9b87461441aa5562ab574bb9eca48d
Nikita <nikitaulyankinipad@gmail.com>
2020-12-17 02:07:18 +0200

    innit commit(probably last one)

---
+++ Assets/Editor/LogicHandlerEditor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(LogicHandler))]
+public class LogicHandlerEditor : Editor
+{
+    private ReorderableList viewlist;
+
+    public void OnEnable()
+    {
+        viewlist = new ReorderableList(serializedObject, serializedObject.FindProperty("stringsList"),
+       false, true, true, true);
+
+        viewlist.drawElementCallback = new ReorderableList.ElementCallbackDelegate(onRedrawElements);
+        viewlist.drawHeaderCallback = new ReorderableList.HeaderCallbackDelegate(onRedrawHeader);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        
+        LogicHandler script = (LogicHandler)target;
+        EditorGUILayout.BeginVertical();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("ContentHolder: ");
+            script.listHolder = (GameObject)EditorGUILayout.ObjectField(script.listHolder,typeof(Object),true);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Row Prefab: ");
+            script.listElementPref = (GameObject)EditorGUILayout.ObjectField(script.listElementPref, typeof(Object), true);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("InputField: ");
+            script.textField = (InputField)EditorGUILayout.ObjectField(script.textField, typeof(InputField), true);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("AddButton: ");
+            script.bttnAdd = (Button)EditorGUILayout.ObjectField(script.bttnAdd, typeof(Button), true);
+            EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+
+        serializedObject.Update();
+        viewlist.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    private void onRedrawElements(Rect rect, int index, bool isActive, bool isFocused) 
+    {
+        var element = viewlist.serializedProperty.GetArrayElementAtIndex(index);
+
+        EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), 
+            element, GUIContent.none);
+    }
+
+    private void onRedrawHeader(Rect rect) 
+    {
+        EditorGUI.LabelField(rect, "List of strings");
+    }
+}
---
+++ Assets/Scripts/LogicHandler.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicHandler : MonoBehaviour
+{
+    private List<string> prevList;
+
+    public List<string> stringsList;
+
+    public GameObject listHolder;
+
+    public GameObject listElementPref;
+
+    public InputField textField;
+
+    public Button bttnAdd;
+
+    void Start()
+    {
+        if (stringsList == null)
+        {
+            stringsList = new List<string>();
+        }
+        prevList = new List<string>();
+
+        bttnAdd.onClick.RemoveAllListeners();
+        bttnAdd.onClick.AddListener(onBttnAddClick);
+    }
+
+    void Update()
+    {
+        if (!prevList.SequenceEqual(stringsList) && stringsList.Count!=0 )
+        {
+            UpdateUi();
+            prevList = new List<string>(stringsList);
+        }
+    }
+
+    private void OnValidate()
+    {
+        UpdateUiInEditor();
+    }
+
+    /// <summary>
+    /// Clear and redraw "table"
+    /// </summary>
+    public void UpdateUi() 
+    {
+        for (int i = 0; i < listHolder.transform.childCount; i++)
+        {
+            var child = listHolder.transform.GetChild(i);
+            if (Application.isPlaying)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        CreateBttns();
+    }
+    
+
+    /// <summary>
+    /// TODO Findout how to get with error
+    /// </summary>
+    private void UpdateUiInEditor() 
+    {
+        if (Application.isEditor)
+        {
+            #if UNITY_EDITOR
+            for (int i = 0; i < listHolder.transform.childCount; i++)
+            {
+                var child = listHolder.transform.GetChild(i);
+                    UnityEditor.EditorApplication.delayCall += () =>
+                    {
+                        DestroyImmediate(child.gameObject);
+                    };
+            }
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+                CreateBttns();
+            };
+            #endif
+        }
+    }
+
+    private void onBttnDelClick(int index) 
+    {
+        stringsList.RemoveAt(index);
+    }
+
+    public void onBttnAddClick() 
+    {
+        stringsList.Add(textField.text);
+        textField.text = "";
+    }
+
+    /// <summary>
+    /// Instantiate raws of the list
+    /// </summary>
+    private void CreateBttns() 
+    {
+        for (int indexer = 0; indexer < stringsList.Count; indexer++)
+        {
+            var raw = Instantiate(listElementPref, listHolder.transform);
+            foreach (Transform child in raw.transform)
+            {
+                if (child.gameObject.name == "Label")
+                {
+                    child.GetChild(0).gameObject.GetComponent<Text>().text = stringsList[indexer];
+                }
+                if (child.gameObject.name == "DellBttn")
+                {
+                    // dont know why but doesnt work without this
+                    int k = indexer;
+
+                    child.gameObject.GetComponent<Button>().onClick.RemoveAllListeners();
+                    child.gameObject.GetComponent<Button>().onClick.AddListener(delegate { onBttnDelClick(k); });
+                }
+            }
+        }
+    }
+}
